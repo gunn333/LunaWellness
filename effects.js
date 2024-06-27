668e8c0851df9cda66dc640ab6f157692501b809
Harleen Kaur <127092049+Harleen365@users.noreply.github.com>
2024-06-27 13:03:38 +0530

    Effects and Management of PCOD/PCOS JS

---
+++ effects.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.classList.add('fade-in');
+    });
+});
+
